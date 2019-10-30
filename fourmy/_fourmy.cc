0c29949ddf73371d81336c320c1ea30cfe3d2bbf
Vincent Mora <vincent.mora@oslandia.com>
2019-10-30 17:35:25 +0100

    removes commented code

--- fourmy/_fourmy.cc
+++ fourmy/_fourmy.cc
@@ -20,7 +20,6 @@ struct FaceInfo2
     }
 };
 
-
 typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
 typedef CGAL::Triangulation_vertex_base_2<K>                      Vb;
 typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo2,K>    Fbb;
@@ -42,7 +41,6 @@ mark_domains(CDT& ct,
     if(start->info().nesting_level != -1){
         return;
     }
-    //std::cout << "mark_domains\n";
     std::list<CDT::Face_handle> queue;
     queue.push_back(start);
     while(! queue.empty()){
@@ -54,17 +52,10 @@ mark_domains(CDT& ct,
                 CDT::Edge e(fh,i);
                 CDT::Face_handle n = fh->neighbor(i);
                 if(n->info().nesting_level == -1){
-                    //if (!ct.is_infinite(e)) std::cout << ct.segment(e);
-                    //else std::cout << "infinit";
                     if(ct.is_constrained(e) && std::find(rings.begin(), rings.end(), ct.segment(e)) != rings.end() ){
-                        //std::cout << (std::find(rings.begin(), rings.end(), 
-                        //            std::make_pair(fh->vertex(i)->point(), fh->vertex((i+1)%3)->point()) 
-                        //            ) != rings.end() ? "ok" : "not ok") << " " << border.size() << "\n";
-                        //std::cout << " constrainted segment\n";
                         border.push_back(e);
                     }
                     else {
-                        //std::cout << " unconstrained\n";
                         queue.push_back(n);
                     }
                 }
@@ -151,14 +142,6 @@ boost::python::object tessellate(const boost::python::object & polygon, const bo
         }
     }
 
-    //for (CDT::Constrained_edges_iterator e=cdt.constrained_edges_begin(); e != cdt.constrained_edges_end(); e++){
-    //    std::cout << cdt.segment(*e);
-    //    if (std::find(border.begin(), border.end(), cdt.segment(*e)) != border.end()){
-    //        std::cout << " border";
-    //    }
-    //    std::cout << "\n";
-    //}
-
     // insert points
     if (points !=  boost::python::object())
     {
@@ -167,15 +150,7 @@ boost::python::object tessellate(const boost::python::object & polygon, const bo
             cdt.push_back(Point(extract<double>(points[i].attr("coords")[0][0]), extract<double>(points[i].attr("coords")[0][1])));
     }
 
-
-    //int i = 0;
-    //for (CDT::Constrained_edges_iterator  e=cdt.constrained_edges_begin(); e != cdt.constrained_edges_end(); ++e, ++i){
-    //    std::cout << i << " " << (std::find(border.begin(), border.end(), *e) != border.end() ? "ring" : "not ring") 
-    //        << " " << (cdt.is_constrained(*e) ? "constrained": "unconstrained") <<"\n";
-    //}
-
     mark_domains(cdt, border);
-
 
     object shapely_geom = import("shapely.geometry");
     list triangles;
@@ -192,7 +167,8 @@ boost::python::object tessellate(const boost::python::object & polygon, const bo
     }
     return shapely_geom.attr("MultiPolygon")(triangles);
 }
-}
+
+} // namespace fourmy
 
 BOOST_PYTHON_FUNCTION_OVERLOADS(tessellate_overloads, fourmy::tessellate, 1, 3)
 
@@ -201,134 +177,3 @@ BOOST_PYTHON_MODULE(_fourmy)
     using namespace boost::python;
     def("tessellate", &fourmy::tessellate, tessellate_overloads(args("polygon", "lines", "points"), "This is tesselate's docstring"));
 }
-
-//    register_exception_translator<std::out_of_range>(translate_out_of_range);
-//
-//    def("from_wkb", &fourmy_from_wkb, return_internal_reference<>());
-//    //def("delete", &fourmy_delete);
-//    //def("tesselate", &fourmy_tesselate, return_internal_reference<>());
-//    
-//    enum_<wkb::byteorder_t>("wkb_byteorder")
-//        .value("big_endian", wkb::big_endian)
-//        .value("little_endian", wkb::little_endian)
-//        ;
-//
-//    enum_<wkb::type_t>("wkb_type")
-//        .value("Point", wkb::point)
-//        .value("LineString", wkb::linestring)
-//        .value("Polygon", wkb::polygon)
-//        .value("MultiPoint", wkb::multipoint)
-//        .value("MultiLineString", wkb::multilinestring)
-//        .value("MultiPolygon", wkb::multipolygon)
-//        .value("GeometryCollection", wkb::geometrycollection)
-//        ;
-//
-//    class_<geometry::point<double>>("Point", init<>())
-//        .def("__len__", &python::point_len) 
-//        .def("__getitem__", &python::point_getitem) 
-//        .def("__repr__", &python::point_repr) 
-//        .add_property("__array_interface__", &python::point_array_interface) 
-//        ;
-//    class_<geometry::linestring<double>>("LineString", init<>())
-//        //.def(vector_indexing_suite<geometry::linestring<double>>() )
-//        .def("__len__", &geometry::linestring<double>::size) 
-//        .def("__getitem__", &python::linestring_getitem) 
-//        .add_property("__array_interface__", &python::linestring_array_interface) 
-//        ;
-//
-//    class_<geometry::polygon<double>>("Polygon", init<>())
-//        .def(vector_indexing_suite<geometry::polygon<double>>() )
-//        ;
-//
-//    class_<geometry::multipolygon<double>>("MultiPolygon", init<>())
-//        .def(vector_indexing_suite<geometry::multipolygon<double>>() )
-//        ;
-//
-//    class_<geometry::geometry<double>>("Geometry", init<>())
-//        ;
-//
-//    def("loads", &python::loads);
-//    def("tessellate", &python::tessellate);
-//}
-
-//void translate_runtime_error(const std::runtime_error & e)
-//{
-//    PyErr_SetString(PyExc_UserWarning, e.what());
-//}
-//
-//
-//struct wrapper
-//{
-//    wrapper(boost::python::object wkb)
-//        : _wkb(PyBytes_Check(wkb.ptr()) ? wkb : wkb.attr("wkb"))
-//    {
-//        _wkb != boost::python::object() || die("cannot create wrapper from None");
-//        // @todo check conformance of object else raise
-//    }
-//
-//    const unsigned char * bytes() const
-//    {
-//        return reinterpret_cast<const unsigned char *>(PyBytes_AsString(_wkb.ptr()));
-//    }
-//
-//private:
-//    // storage is either const from a python object, or non const from other ctor
-//    const boost::python::object _wkb;
-//
-//};
-//
-    //class_<geometry::geometry>("Geometry", init<object>())
-    //    .add_property("type", &geometry::geometry::type)
-    //    .add_property("has_z", &geometry::geometry::has_z)
-    //    .add_property("has_srid", &geometry::geometry::has_srid)
-    //    .add_property("has_m", &geometry::geometry::has_m)
-    //    .add_property("srid", &geometry::geometry::srid)
-    //    .add_property("wkb", &geometry::geometry::wkb)
-    //    ;
-        //.def("__len__", &geometry::point<double>::size)
-        //.def("__getitem__", const double &(geometry::point<double>::*operator{])() =  &geometry::point<double>::operator[])
-    //    ;
-
-
-//    class_<geometry::coordinate>("Coordinate", no_init)
-//        .def("__len__", &geometry::coordinate::size)
-//        .def("__getitem__", &geometry::coordinate::operator[])
-//        ;
-//
-//    class_<geometry::coordinate_sequence>("CoordinateSequence", no_init)
-//        .def("__len__", &geometry::coordinate_sequence::size)
-//        .def("__getitem__", &geometry::coordinate_sequence::operator[])
-//        ;
-//
-//    class_<geometry::point, bases<geometry::geometry> >("Point", init<object>())
-//        //.add_property("x", &geometry::point::x)
-//        //.add_property("y", &geometry::point::y)
-//        //.add_property("z", &geometry::point::z)
-//        //.add_property("m", &geometry::point::m)
-//        .add_property("coords", &geometry::point::coords)
-//        ;
-//
-//    class_<geometry::linestring, bases<geometry::geometry> >("LineString", init<object>())
-//        .add_property("coords", &geometry::linestring::coords)
-//        ;
-
-
-    //def("delaunay", delaunay::tessellate);
-
-//    class_<boolean::Mesh>("Mesh", init<>())
-//        .add_property("number_of_vertices", &boolean::Mesh::number_of_vertices)
-//        .add_property("number_of_faces", &boolean::Mesh::number_of_faces);
-//    def("union", boolean::union_);
-//    def("create_mesh", boolean::create_mesh);
-//
-//    class_<geometry::CoordinateSequence>("CoordinateSequence", init<>())
-//        .add_property("__getitem__", &geometry::CoordinateSequence::operator[])
-//        .add_property("__len__", &geometry::CoordinateSequence::size)
-//        ;
-//boost::python::object do_it(const boost::python::object in)
-//{
-//    const char *data = PyBytes_AsString(in.ptr());
-//    const std::vector<unsigned char> v(data, data+PyBytes_Size(in.ptr()));
-//    return boost::python::object(boost::python::handle<>(PyBytes_FromStringAndSize(reinterpret_cast<const char*>(&v[0]), v.size())));
-//}
-//
